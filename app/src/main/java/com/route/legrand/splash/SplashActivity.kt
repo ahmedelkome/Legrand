a3726ea9e804317c1acada84cea7dd682361e27b
ahmed elkomy <ahmedelkomy@gmail.com>
2024-08-10 14:52:37 +0300

    Create Splash Screen & navigate to home

---
+++ app/src/main/java/com/route/legrand/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.route.legrand.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.route.legrand.R
+import com.route.legrand.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
