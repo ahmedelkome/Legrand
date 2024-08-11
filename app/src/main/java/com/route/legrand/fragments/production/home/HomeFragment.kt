ead73a7b9ee200a35adc0d7c7132c53f51cf49d2
ahmed elkomy <ahmedelkomy@gmail.com>
2024-08-11 15:35:53 +0300

    create home production fragment

---
+++ app/src/main/java/com/route/legrand/fragments/production/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.route.legrand.fragments.production.home
+
+import com.route.legrand.base.BaseFragment
+import com.route.legrand.databinding.HomeFragmentProductionBinding
+
+class HomeFragment : BaseFragment<HomeFragmentProductionBinding>() {
+    override fun observeLiveData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLayout(): Int {
+        TODO("Not yet implemented")
+    }
+}
