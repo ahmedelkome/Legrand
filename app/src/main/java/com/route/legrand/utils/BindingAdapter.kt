47aacf767a3763435a244695ca60e95b2edfda3a
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-08-11 18:53:41 +0300

    define BindingAdapter To TextInput layout

---
+++ app/src/main/java/com/route/legrand/utils/BindingAdapter.kt
@@ -0,0 +1,9 @@
+package com.route.legrand.utils
+
+import androidx.databinding.BindingAdapter
+import com.google.android.material.textfield.TextInputLayout
+
+@BindingAdapter("error")
+fun BindingAdapterError(textInputLayout: TextInputLayout, error: String?) {
+    textInputLayout.error = error
+}
