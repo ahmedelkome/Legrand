90e7147c3529b7bd7f60bee334b6d58e6f9cf2e6
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-08-12 18:47:36 +0300

    create injection fragment

---
+++ app/src/main/java/com/route/legrand/fragments/production/injection/InjectionFragment.kt
@@ -0,0 +1,15 @@
+package com.route.legrand.fragments.production.injection
+
+import com.route.legrand.R
+import com.route.legrand.base.BaseFragment
+import com.route.legrand.databinding.FragmentInjectionBinding
+
+
+class InjectionFragment : BaseFragment<FragmentInjectionBinding>() {
+    override fun observeLiveData() {
+
+    }
+
+    override fun getLayout(): Int = R.layout.fragment_injection
+
+}
