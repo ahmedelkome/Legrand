f218eafd253c19e71bd1101185a8625f0cf8644a
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-08-24 18:56:49 +0300

    Add firebase storage dependency

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.firebase.bom)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
 
     //Hilt
     implementation(libs.hilt.android)
