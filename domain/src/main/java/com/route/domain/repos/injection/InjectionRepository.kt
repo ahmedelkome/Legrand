af5e021efa068e4a8f842277abd3f5f61a21e6b9
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-08-24 19:01:13 +0300

    injection domain ready to get parameter sheet

--- domain/src/main/java/com/route/domain/repos/injection/InjectionRepository.kt
+++ domain/src/main/java/com/route/domain/repos/injection/InjectionRepository.kt
@@ -3,8 +3,11 @@ package com.route.domain.repos.injection
 import com.route.domain.common.ResultWrapper
 import com.route.domain.models.injection.InjectionData
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 
 interface InjectionRepository {
 
     suspend fun getInjectionData(): Flow<ResultWrapper<List<InjectionData>>>
+
+    suspend fun getParameterSheet(partNumber:String):Flow<ResultWrapper<File?>>
 }
--- domain/src/main/java/com/route/domain/usecases/injection/InjectionUseCase.kt
+++ domain/src/main/java/com/route/domain/usecases/injection/InjectionUseCase.kt
@@ -4,6 +4,7 @@ import com.route.domain.common.ResultWrapper
 import com.route.domain.models.injection.InjectionData
 import com.route.domain.repos.injection.InjectionRepository
 import kotlinx.coroutines.flow.Flow
+import java.io.File
 import javax.inject.Inject
 
 class InjectionUseCase @Inject constructor(
@@ -11,5 +12,9 @@ class InjectionUseCase @Inject constructor(
 ) {
     suspend fun execute(): Flow<ResultWrapper<List<InjectionData>>> {
         return injectionRepository.getInjectionData()
+    }
+
+    suspend fun getParameterSheet(partNumber: String): Flow<ResultWrapper<File?>> {
+        return injectionRepository.getParameterSheet(partNumber)
     }
 }
